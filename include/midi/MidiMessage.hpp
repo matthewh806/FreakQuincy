116357fd6c881b72cfd36594fc6bd5f5571efe73
matthewh806 <matthewh806@gmail.com>
2019-07-09 16:50:00 +0200

    implement some midi message methods

--- include/midi/MidiMessage.hpp
+++ include/midi/MidiMessage.hpp
@@ -4,9 +4,37 @@
 #include <iostream>
 
 namespace midi {
+
+    enum StatusType {
+        NOTE_ON = 0x9,
+        NOTE_OFF = 0x8,
+        UNKNOWN = 0x0 // check if this is sensible...
+    };
+
     struct MidiMessage {
         uint8_t size = 3;
         uint8_t bytes[3] = {};
+
+        uint8_t getChannel() {
+            return bytes[0] & 0x0f;
+        }
+
+        uint8_t getStatus() {
+            return bytes[0] >> 4;
+        }
+
+        StatusType getStatusType() {
+            return (StatusType)getStatus();
+        }
+
+        uint8_t getNote() {
+            return bytes[1];
+        }
+
+        // e.g. velocity
+        uint8_t getValue() {
+            return bytes[2];
+        }
 
         void printBinary() {
             for(unsigned int i=0; i<size; i++) {
