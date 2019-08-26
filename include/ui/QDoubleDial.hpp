82098b708e5e91120530b17c277afe4ecf6e76ea
matthewh806 <matthewh806@gmail.com>
2019-08-26 13:10:14 +0200

    custom dial working roughly

--- include/ui/QDoubleDial.hpp
+++ include/ui/QDoubleDial.hpp
@@ -2,23 +2,100 @@
 #define QDOUBLEDIAL_HPP
 
 #include <math.h>
+
+#include <iostream>
+
 #include <QDial>
+#include <QSharedPointer>
+#include <QRectF>
+#include <QColor>
+#include <QPen>
+
+/*
+* Layout specific implementation mostly comes from this blog post:
+* http://thecodeinn.blogspot.com/2015/03/customizing-qdials-in-qt-part-2.html
+*/
 
 namespace ui {
 
     class QDoubleDial : public QDial {
         Q_OBJECT
 
+        Q_PROPERTY(QString arcColor READ getArcColor WRITE setArcColor)
+
+        Q_PROPERTY(double arcWidth READ getArcWidth WRITE setArcWidth)
+
         public:
-            QDoubleDial(QWidget *parent = nullptr, int mag = 1) : QDial(parent) {
+            QDoubleDial(QWidget *parent = nullptr, int mag = 1) : 
+            QDial(parent),
+            m_arcRect(new QRectF),
+            m_valueRect(new QRectF),
+            m_arcColor(new QColor),
+            m_arcPen(new QPen)
+            {
                 this->m_multi = pow(10, mag);
 
-                connect(this, SIGNAL(valueChanged(int)),
-                    this, SLOT(notifyValueChanged(int))
+                QDial::setCursor(Qt::PointingHandCursor);
+
+                connect(this, &QDial::valueChanged,
+                    this, &QDoubleDial::notifyValueChanged
                 );
+
+                setMinimumSize(100, 100);
+                setMaximumSize(100, 100);
+                setMaximumAngle(-360);
+
+                // QSizePolicy* verticalPol = new QSizePolicy();
+                // verticalPol->setWidthForHeight(true);
+                // QSizePolicy sizePolicy = this->sizePolicy();
+                // sizePolicy.setVerticalPolicy(verticalPol->);
+
+                std::cout << QDial::width() << ", " << QDial::height() << std::endl;
             };
 
             ~QDoubleDial() {};
+
+            void setArcColor(const QString& color) {
+                m_arcColor->setNamedColor(color);
+                m_arcPen->setColor(*m_arcColor);
+            }
+
+            QString getArcColor() const {
+                return m_arcColor->name();
+            }
+
+            void setStartAngle(double angle) {
+                m_startAngle = angle * 16;
+            }
+
+            double getStartAngle() const {
+                return m_startAngle / 16;
+            }
+
+            void setMaximumAngle(double angle) {
+                m_maximumAngleSpan = angle * 16;
+            }
+
+            double getMaximumAngle() const {
+                return m_maximumAngleSpan / 16;
+            }
+
+            void setArcWidth(double px) {
+                m_arcWidth = px;
+                
+                *m_arcRect = QRectF(
+                    m_arcWidth / 2,
+                    m_arcWidth / 2,
+                    QDial::width() - m_arcWidth,
+                    QDial::height() - m_arcWidth
+                );
+
+                m_arcPen->setWidth(m_arcWidth);
+            }
+
+            double getArcWidth() const {
+                return m_arcWidth;
+            }
 
             void setMinimum(double val) {
                 QDial::setMinimum(val * this->m_multi);
@@ -34,6 +111,9 @@ namespace ui {
 
             void setValue(double val) {
                 QDial::setValue(val * this->m_multi);
+
+                double ratio = QDial::value() / (double)QDial::maximum();
+                setStartAngle(m_maximumAngleSpan * ratio);
             }
 
             void setMagnitude(int mag) {
@@ -44,12 +124,25 @@ namespace ui {
             void doubleValueChanged(double value);
 
         public slots:
-            void notifyValueChanged(int value) {
-                emit doubleValueChanged(value / (double)m_multi);
-        }
+            void notifyValueChanged(int value);
 
         private:
+            virtual void paintEvent(QPaintEvent*) override;
+            virtual void resizeEvent(QResizeEvent* event) override;
+
             int m_multi;
+
+            double m_maximumAngleSpan;
+            double m_startAngle;
+            double m_arcWidth;
+            double m_angleSpan;
+
+            QString m_valueString;
+
+            QSharedPointer<QRectF> m_arcRect;
+            QSharedPointer<QRectF> m_valueRect;
+            QSharedPointer<QColor> m_arcColor;
+            QSharedPointer<QPen> m_arcPen;
     };
 }
 
---
+++ src/ui/QDoubleDial.cpp
@@ -0,0 +1,49 @@
+#include "ui/QDoubleDial.hpp"
+
+#include <QPainter>
+#include <QResizeEvent>
+
+namespace ui {
+    void QDoubleDial::paintEvent(QPaintEvent *) {
+        QPainter painter(this);
+        painter.setBackgroundMode(Qt::OpaqueMode);
+        painter.setRenderHint(QPainter::Antialiasing);
+        painter.setBrush(painter.background());
+
+        QPen textPen = painter.pen();
+        painter.setPen(QPen(Qt::NoPen));
+        painter.drawEllipse(QDial::rect());
+
+        painter.setPen(textPen);
+        painter.drawText(*m_valueRect, Qt::AlignCenter, m_valueString);
+
+        painter.setPen(*m_arcPen);
+        painter.drawArc(*m_arcRect, m_startAngle, m_angleSpan);
+    }
+
+    void QDoubleDial::resizeEvent(QResizeEvent* event) {
+        std::cout << "Size: " << event->size().width() << ", " << event->size().height() << std::endl;
+        QDial::setMinimumSize(event->size());
+        
+        double w = QDial::width() - (2 * m_arcWidth);
+        double h = w / 2;
+
+        *m_valueRect = QRectF(m_arcWidth, h, w, h);
+        *m_arcRect = QRectF(
+            m_arcWidth / 2,
+            m_arcWidth / 2,
+            QDial::width() - m_arcWidth,
+            QDial::height() - m_arcWidth
+        );
+    }
+
+    void QDoubleDial::notifyValueChanged(int value) {
+        double val = value / (double)m_multi;
+
+        emit doubleValueChanged(val);
+
+        double ratio = QDial::value() / (double)QDial::maximum();
+        m_angleSpan = m_maximumAngleSpan * ratio;
+        m_valueString = QString::number(val);
+    }
+}
