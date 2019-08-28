6bbff736064a0481be2b96d043f4cfa8986fe704
matthewh806 <matthewh806@gmail.com>
2019-08-28 17:03:58 +0200

    uncomment accidentally commented code

--- src/engine/LinearTimeADSR.cpp
+++ src/engine/LinearTimeADSR.cpp
@@ -2,30 +2,30 @@
 
 namespace engine {
 
-    // void LinearTimeADSR::notePressed(float freq, bool legato) {
-    //     if(legato)
-    //         return;
+    void LinearTimeADSR::notePressed(float freq, bool legato) {
+        if(legato)
+            return;
 
-    //     m_state = 0.0;
+        m_state = 0.0;
 
-    //     /*
-    //         TODO: Ramp down does not actually go to zero 
-    //         The value gets closer to zero - and then attack 
-    //         just starts from that value again
+        /*
+            TODO: Ramp down does not actually go to zero 
+            The value gets closer to zero - and then attack 
+            just starts from that value again
 
-    //         maybe its not noticeable as the value tends to be < 10^-4
-    //         but the higher the value the ramp down begins from
-    //         the further from 0 it will actually be after the 0.03 seconds 
-    //         have elapsed. 
-    //     */
-    //     if(m_curParamVal > 0.0) {
-    //         m_stage = RAMP_DOWN;
-    //         m_prevParamVal = m_curParamVal;
-    //     } else {
-    //         m_stage = ATTACK;
-    //         m_prevParamVal = 0.0;
-    //     }
-    // }
+            maybe its not noticeable as the value tends to be < 10^-4
+            but the higher the value the ramp down begins from
+            the further from 0 it will actually be after the 0.03 seconds 
+            have elapsed. 
+        */
+        if(m_curParamVal > 0.0) {
+            m_stage = RAMP_DOWN;
+            m_prevParamVal = m_curParamVal;
+        } else {
+            m_stage = ATTACK;
+            m_prevParamVal = 0.0;
+        }
+    }
 
     void LinearTimeADSR::noteReleased(bool legato) {
         if(m_stage >= RELEASE || legato)
