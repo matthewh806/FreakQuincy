ee7d96e84255e9055a1882edeb7529cc08b0a96b
matthewh806 <matthewh806@gmail.com>
2019-09-05 13:20:17 +0200

    test added to makefile

--- lib/tests/FilterTestCases.cpp
+++
@@ -1,8 +0,0 @@
-#include "catch.hpp"
-#include "engine/filters/SecondOrderButterworthLowPassFilter.hpp"
-
-SecondOrderButterworthLowPassFilter filter = new SecondOrderButterworthLowPassFilter(1000, 0.7);
-
-TEST_CASE( "Cutoff Freq is 1000", "[Filter]") {
-    REQUIRE( filter->getCutoff() == 1000);
-}
